56d94b17551d1701e6ffc27b5bc62b4d8559082a
Zibi Braniecki <gandalf@mozilla.com>
2018-04-28 22:34:12 -0700

    Remove FormattedDisplayList

--- src/format.rs
+++ src/format.rs
@@ -1,8 +1,6 @@
 use std::fmt;
 use structs::display_list::{DisplayAnnotationType, DisplayLine, DisplayList, DisplayMark,
                             DisplaySnippetType};
-use structs::formatted_display_list::{FormattedDisplayLine, FormattedDisplayList};
-
 struct DisplayListFormatting {}
 
 impl DisplayListFormatting {
@@ -26,101 +24,6 @@ impl DisplayListFormatting {
                 .join(""),
             width = inline_marks_width
         )
-    }
-}
-
-impl fmt::Display for DisplayList {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let lineno_width = self.body.iter().fold(0, |max, line| match line {
-            DisplayLine::Source { lineno, .. } => {
-                let width = lineno.to_string().len();
-                if width > max {
-                    width
-                } else {
-                    max
-                }
-            }
-            _ => max,
-        });
-        let inline_marks_width = self.body.iter().fold(0, |max, line| match line {
-            DisplayLine::Source { inline_marks, .. } => {
-                let width = inline_marks.len();
-                if width > max {
-                    width + 1
-                } else {
-                    max
-                }
-            }
-            _ => max,
-        });
-        let body = self.body
-            .clone()
-            .into_iter()
-            .map(|line| FormattedDisplayLine::format(line, lineno_width, inline_marks_width))
-            .collect();
-        let fdl = FormattedDisplayList { body };
-        write!(f, "{}", fdl)
-    }
-}
-
-impl fmt::Display for FormattedDisplayList {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        if let Some((last, elements)) = self.body.split_last() {
-            for line in elements.iter() {
-                line.fmt(f)?;
-                writeln!(f)?;
-            }
-            last.fmt(f)?;
-        }
-        Ok(())
-    }
-}
-
-impl FormattedDisplayLine {
-    pub fn format(dl: DisplayLine, lineno_width: usize, inline_marks_width: usize) -> Self {
-        match dl {
-            DisplayLine::Description {
-                snippet_type,
-                id,
-                label,
-            } => FormattedDisplayLine::Raw(format!(
-                "{}[{}]: {}",
-                DisplayListFormatting::format_snippet_type(&snippet_type),
-                id,
-                label
-            )),
-            DisplayLine::Origin { path, row, col } => FormattedDisplayLine::Raw(format!(
-                "{}--> {}:{}:{}",
-                " ".repeat(lineno_width),
-                path,
-                row,
-                col
-            )),
-            DisplayLine::EmptySource => FormattedDisplayLine::EmptySource {
-                lineno: " ".repeat(lineno_width),
-            },
-            DisplayLine::Source {
-                lineno,
-                inline_marks,
-                content,
-                ..
-            } => FormattedDisplayLine::Source {
-                lineno: format!("{: >width$}", lineno, width = lineno_width),
-                inline_marks: DisplayListFormatting::format_inline_marks(&inline_marks, inline_marks_width),
-                content,
-            },
-            DisplayLine::Annotation {
-                inline_marks,
-                range,
-                label,
-                annotation_type,
-            } => FormattedDisplayLine::Annotation {
-                lineno: " ".repeat(lineno_width),
-                inline_marks: DisplayListFormatting::format_inline_marks(&inline_marks, inline_marks_width),
-                content: Self::format_annotation_content(range, &label, &annotation_type),
-            },
-            DisplayLine::Fold => FormattedDisplayLine::Fold,
-        }
     }
 
     fn format_annotation_content(
@@ -156,24 +59,96 @@ impl FormattedDisplayLine {
             ),
         }
     }
+
+    pub fn format_line(dl: DisplayLine, lineno_width: usize, inline_marks_width: usize) -> String {
+        match dl {
+            DisplayLine::Description {
+                snippet_type,
+                id,
+                label,
+            } => format!(
+                "{}[{}]: {}",
+                DisplayListFormatting::format_snippet_type(&snippet_type),
+                id,
+                label
+            ),
+            DisplayLine::Origin { path, row, col } => format!(
+                "{}--> {}:{}:{}",
+                " ".repeat(lineno_width),
+                path,
+                row,
+                col
+            ),
+            DisplayLine::EmptySource => format!(
+                "{} |",
+                " ".repeat(lineno_width)
+            ),
+            DisplayLine::Source {
+                lineno,
+                inline_marks,
+                content,
+                ..
+            } => format!(
+                "{:>width$} |{} {}",
+                lineno,
+                DisplayListFormatting::format_inline_marks(&inline_marks, inline_marks_width),
+                content,
+                width = lineno_width,
+            ),
+//             FormattedDisplayLine::Annotation {
+//                 lineno,
+//                 inline_marks,
+//                 content,
+//             } => write!(f, "{} |{}{}", lineno, inline_marks, content),
+//             FormattedDisplayLine::Fold => write!(f, "...  |"),
+            DisplayLine::Annotation {
+                inline_marks,
+                range,
+                label,
+                annotation_type,
+            } => format!(
+                "{} |{}{}",
+                " ".repeat(lineno_width),
+                DisplayListFormatting::format_inline_marks(&inline_marks, inline_marks_width),
+                DisplayListFormatting::format_annotation_content(range, &label, &annotation_type),
+            ),
+            DisplayLine::Fold => format!(
+                "...  |",
+            ),
+        }
+    }
 }
 
-impl fmt::Display for FormattedDisplayLine {
+impl fmt::Display for DisplayList {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            FormattedDisplayLine::EmptySource { lineno } => write!(f, "{} |", lineno),
-            FormattedDisplayLine::Source {
-                lineno,
-                inline_marks,
-                content,
-            } => write!(f, "{} |{} {}", lineno, inline_marks, content),
-            FormattedDisplayLine::Raw(body) => write!(f, "{}", body),
-            FormattedDisplayLine::Annotation {
-                lineno,
-                inline_marks,
-                content,
-            } => write!(f, "{} |{}{}", lineno, inline_marks, content),
-            FormattedDisplayLine::Fold => write!(f, "...  |"),
-        }
+        let lineno_width = self.body.iter().fold(0, |max, line| match line {
+            DisplayLine::Source { lineno, .. } => {
+                let width = lineno.to_string().len();
+                if width > max {
+                    width
+                } else {
+                    max
+                }
+            }
+            _ => max,
+        });
+        let inline_marks_width = self.body.iter().fold(0, |max, line| match line {
+            DisplayLine::Source { inline_marks, .. } => {
+                let width = inline_marks.len();
+                if width > max {
+                    width + 1
+                } else {
+                    max
+                }
+            }
+            _ => max,
+        });
+        let body = self.body
+            .clone()
+            .into_iter()
+            .map(|line| DisplayListFormatting::format_line(line, lineno_width, inline_marks_width))
+            .collect::<Vec<String>>();
+        // let fdl = FormattedDisplayList { body };
+        write!(f, "{}", body.join("\n"))
     }
 }
--- src/structs/mod.rs
+++ src/structs/mod.rs
@@ -1,3 +1,2 @@
 pub mod display_list;
-pub mod formatted_display_list;
 pub mod snippet;
