ad801833687d72c45f1346b402bba32afc6bb28e
Scott Schafer <schaferjscott@gmail.com>
2024-03-02 16:24:16 -0700

    refactor(fixtures): Move to snapbox with SVGs

---
+++ tests/fixtures/main.rs
@@ -0,0 +1,33 @@
+mod deserialize;
+
+use crate::deserialize::Fixture;
+use annotate_snippets::{Renderer, Snippet};
+use snapbox::data::DataFormat;
+use snapbox::Data;
+use std::error::Error;
+
+fn main() {
+    #[cfg(not(windows))]
+    snapbox::harness::Harness::new("tests/fixtures/", setup, test)
+        .select(["*/*.toml"])
+        .action_env("SNAPSHOTS")
+        .test();
+}
+
+fn setup(input_path: std::path::PathBuf) -> snapbox::harness::Case {
+    let name = input_path.file_name().unwrap().to_str().unwrap().to_owned();
+    let expected = input_path.with_extension("svg");
+    snapbox::harness::Case {
+        name,
+        fixture: input_path,
+        expected,
+    }
+}
+
+fn test(input_path: &std::path::Path) -> Result<Data, Box<dyn Error>> {
+    let src = std::fs::read_to_string(input_path)?;
+    let (renderer, snippet): (Renderer, Snippet<'_>) =
+        toml::from_str(&src).map(|a: Fixture| (a.renderer.into(), a.snippet.into()))?;
+    let actual = renderer.render(snippet).to_string();
+    Ok(Data::from(actual).coerce_to(DataFormat::TermSvg))
+}
