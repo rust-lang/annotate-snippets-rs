aed54bb789f9830996d08181634542aa0b53b1b8
Scott Schafer <schaferjscott@gmail.com>
2024-03-10 12:45:03 -0600

    fix: Make spans work with single width emojis

--- tests/formatter.rs
+++ tests/formatter.rs
@@ -25,7 +25,7 @@ fn test_point_to_double_width_characters() {
     let snippets = Level::Error.title("").snippet(
         Snippet::source("こんにちは、世界")
             .origin("<current file>")
-            .annotation(Level::Error.span(12..16).label("world")),
+            .annotation(Level::Error.span(18..24).label("world")),
     );
 
     let expected = r#"error
@@ -44,7 +44,7 @@ fn test_point_to_double_width_characters_across_lines() {
     let snippets = Level::Error.title("").snippet(
         Snippet::source("おはよう\nございます")
             .origin("<current file>")
-            .annotation(Level::Error.span(4..15).label("Good morning")),
+            .annotation(Level::Error.span(6..22).label("Good morning")),
     );
 
     let expected = r#"error
@@ -65,8 +65,8 @@ fn test_point_to_double_width_characters_multiple() {
     let snippets = Level::Error.title("").snippet(
         Snippet::source("お寿司\n食べたい🍣")
             .origin("<current file>")
-            .annotation(Level::Error.span(0..6).label("Sushi1"))
-            .annotation(Level::Note.span(11..15).label("Sushi2")),
+            .annotation(Level::Error.span(0..9).label("Sushi1"))
+            .annotation(Level::Note.span(16..22).label("Sushi2")),
     );
 
     let expected = r#"error
@@ -87,7 +87,7 @@ fn test_point_to_double_width_characters_mixed() {
     let snippets = Level::Error.title("").snippet(
         Snippet::source("こんにちは、新しいWorld！")
             .origin("<current file>")
-            .annotation(Level::Error.span(12..23).label("New world")),
+            .annotation(Level::Error.span(18..32).label("New world")),
     );
 
     let expected = r#"error
