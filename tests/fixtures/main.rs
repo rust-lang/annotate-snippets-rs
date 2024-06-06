b5a6b53ecd4a377df293ea85fb695f1137573404
Ed Page <eopage@gmail.com>
2024-06-06 13:59:28 -0500

    chore: Update from epage/_rust template

--- tests/fixtures/main.rs
+++ tests/fixtures/main.rs
@@ -26,7 +26,7 @@ fn setup(input_path: std::path::PathBuf) -> tryfn::Case {
 fn test(input_path: &std::path::Path) -> Result<Data, Box<dyn Error>> {
     let src = std::fs::read_to_string(input_path)?;
     let (renderer, message): (Renderer, Message<'_>) =
-        toml::from_str(&src).map(|a: Fixture| (a.renderer.into(), a.message.into()))?;
+        toml::from_str(&src).map(|a: Fixture<'_>| (a.renderer.into(), a.message.into()))?;
     let actual = renderer.render(message).to_string();
     Ok(Data::from(actual).coerce_to(DataFormat::TermSvg))
 }
