d44f854770b6873f75770c018ee445e8589f70e0
Tirth Patel <tirthpatel204@gmail.com>
2025-03-17 23:56:38 +0530

    refactor(#184)

---
+++ src/renderer/display/constants.rs
@@ -0,0 +1,6 @@
+pub(crate) const ANONYMIZED_LINE_NUM: &str = "LL";
+pub(crate) const ERROR_TXT: &str = "error";
+pub(crate) const HELP_TXT: &str = "help";
+pub(crate) const INFO_TXT: &str = "info";
+pub(crate) const NOTE_TXT: &str = "note";
+pub(crate) const WARNING_TXT: &str = "warning";
