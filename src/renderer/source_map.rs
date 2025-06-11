d4300058bd506fb83683bb8f4ca1275760bf03a8
Scott Schafer <schaferjscott@gmail.com>
2025-06-11 09:39:53 -0600

    chore: Fix clippy warning

--- src/renderer/source_map.rs
+++ src/renderer/source_map.rs
@@ -298,9 +298,9 @@ impl<'a> SourceMap<'a> {
             annotated_line_infos.retain(|l| !l.annotations.is_empty());
         }
 
-        annotated_line_infos
-            .iter_mut()
-            .for_each(|l| l.annotations.sort_by(|a, b| a.start.cmp(&b.start)));
+        for l in annotated_line_infos.iter_mut() {
+            l.annotations.sort_by(|a, b| a.start.cmp(&b.start));
+        }
 
         (max_depth, annotated_line_infos)
     }
