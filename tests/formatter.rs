5cee9d38993d049b986ed1223c197e74dbc64cfc
Scott Schafer <schaferjscott@gmail.com>
2025-06-12 08:44:03 -0600

    test: Add a test for id on any Title

--- tests/formatter.rs
+++ tests/formatter.rs
@@ -2587,3 +2587,115 @@ LL |     let s: &str = include_bytes!("file.txt");   //~ ERROR mismatched types
     let renderer = Renderer::plain().anonymized_line_numbers(true);
     assert_data_eq!(renderer.render(input), expected);
 }
+
+#[test]
+fn id_on_title() {
+    let source = r#"// Regression test for issue #114529
+// Tests that we do not ICE during const eval for a
+// break-with-value in contexts where it is illegal
+
+#[allow(while_true)]
+fn main() {
+    [(); {
+        while true {
+            break 9; //~ ERROR `break` with value from a `while` loop
+        };
+        51
+    }];
+
+    [(); {
+        while let Some(v) = Some(9) {
+            break v; //~ ERROR `break` with value from a `while` loop
+        };
+        51
+    }];
+
+    while true {
+        break (|| { //~ ERROR `break` with value from a `while` loop
+            let local = 9;
+        });
+    }
+}
+"#;
+    let input = Level::ERROR
+        .header("`break` with value from a `while` loop")
+        .id("E0571")
+        .group(
+            Group::new().element(
+                Snippet::source(source)
+                    .line_start(1)
+                    .path("$DIR/issue-114529-illegal-break-with-value.rs")
+                    .fold(true)
+                    .annotation(
+                        AnnotationKind::Primary
+                            .span(483..581)
+                            .label("can only break with a value inside `loop` or breakable block"),
+                    )
+                    .annotation(
+                        AnnotationKind::Context
+                            .span(462..472)
+                            .label("you can't `break` with a value in a `while` loop"),
+                    ),
+            ),
+        )
+        .group(
+            Group::new()
+                .element(
+                    Level::HELP
+                        .text(Some("suggestion[S0123]"))
+                        .title("use `break` on its own without a value inside this `while` loop"),
+                )
+                .element(
+                    Snippet::source(source)
+                        .line_start(1)
+                        .path("$DIR/issue-114529-illegal-break-with-value.rs")
+                        .fold(true)
+                        .patch(Patch::new(483..581, "break")),
+                ),
+        );
+
+    let expected_ascii = str![[r#"
+error[E0571]: `break` with value from a `while` loop
+  --> $DIR/issue-114529-illegal-break-with-value.rs:22:9
+   |
+LL |       while true {
+   |       ---------- you can't `break` with a value in a `while` loop
+LL | /         break (|| { //~ ERROR `break` with value from a `while` loop
+LL | |             let local = 9;
+LL | |         });
+   | |__________^ can only break with a value inside `loop` or breakable block
+   |
+suggestion[S0123]: use `break` on its own without a value inside this `while` loop
+   |
+LL -         break (|| { //~ ERROR `break` with value from a `while` loop
+LL -             let local = 9;
+LL -         });
+LL +         break;
+   |
+"#]];
+
+    let renderer_ascii = Renderer::plain().anonymized_line_numbers(true);
+    assert_data_eq!(renderer_ascii.render(input.clone()), expected_ascii);
+
+    let expected_unicode = str![[r#"
+error[E0571]: `break` with value from a `while` loop
+   ╭▸ $DIR/issue-114529-illegal-break-with-value.rs:22:9
+   │
+LL │       while true {
+   │       ────────── you can't `break` with a value in a `while` loop
+LL │ ┏         break (|| { //~ ERROR `break` with value from a `while` loop
+LL │ ┃             let local = 9;
+LL │ ┃         });
+   │ ┗━━━━━━━━━━┛ can only break with a value inside `loop` or breakable block
+   ╰╴
+suggestion[S0123]: use `break` on its own without a value inside this `while` loop
+   ╭╴
+LL -         break (|| { //~ ERROR `break` with value from a `while` loop
+LL -             let local = 9;
+LL -         });
+LL +         break;
+   ╰╴
+"#]];
+    let renderer_unicode = renderer_ascii.theme(OutputTheme::Unicode);
+    assert_data_eq!(renderer_unicode.render(input), expected_unicode);
+}
