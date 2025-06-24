52fd5df0e0c3f3faf5ddc1ea3fdf3c88a500ddeb
Scott Schafer <schaferjscott@gmail.com>
2025-06-24 03:30:20 -0600

    test: Add a test for a group with no Title

---
+++ examples/elide_header.rs
@@ -0,0 +1,21 @@
+use annotate_snippets::{AnnotationKind, Group, Level, Renderer, Snippet};
+
+fn main() {
+    let source = r#"# Docstring followed by a newline
+
+def foobar(door, bar={}):
+    """
+    """
+"#;
+
+    let message = &[Group::new()
+        .element(
+            Snippet::source(source)
+                .fold(false)
+                .annotation(AnnotationKind::Primary.span(56..58).label("B006")),
+        )
+        .element(Level::HELP.title("Replace with `None`; initialize within function"))];
+
+    let renderer = Renderer::styled();
+    anstream::println!("{}", renderer.render(message));
+}
