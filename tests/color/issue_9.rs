3dd4ba67bfbd8b9965e4575f2fa108883f4b9f2e
Scott Schafer <schaferjscott@gmail.com>
2025-07-31 10:05:55 -0600

    test: Make issue_9 more closely match its original output

--- tests/color/issue_9.rs
+++ tests/color/issue_9.rs
@@ -4,22 +4,22 @@ use snapbox::{assert_data_eq, file};
 
 #[test]
 fn case() {
+    let source = r#"let x = vec![1];
+
+
+let y = x;
+
+x;
+"#;
+
     let input = &[Group::with_title(Level::ERROR.title("expected one of `.`, `;`, `?`, or an operator, found `for`"))
         .element(
-            Snippet::source("let x = vec![1];")
+            Snippet::source(source)
                 .path("/code/rust/src/test/ui/annotate-snippet/suggestion.rs")
                 .line_start(4)
                 .annotation(AnnotationKind::Context.span(4..5).label("move occurs because `x` has type `std::vec::Vec<i32>`, which does not implement the `Copy` trait"))
-        )
-        .element(
-            Snippet::source("let y = x;")
-                .line_start(7)
-                .annotation(AnnotationKind::Context.span(8..9).label("value moved here"))
-        )
-        .element(
-            Snippet::source("x;")
-                .line_start(9)
-                .annotation(AnnotationKind::Primary.span(0..1).label("value used here after move"))
+                .annotation(AnnotationKind::Context.span(27..28).label("value moved here"))
+                .annotation(AnnotationKind::Primary.span(31..32).label("value used here after move"))
         )
     ];
     let expected = file!["issue_9.term.svg"];
