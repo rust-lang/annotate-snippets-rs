93b2d433fa9a9ac8ddf78652e48b94ac663a0ba2
Scott Schafer <schaferjscott@gmail.com>
2026-05-05 14:27:03 -0600

    refactor: Remove unused line counting from push_trailing

--- src/renderer/source_map.rs
+++ src/renderer/source_map.rs
@@ -380,13 +380,7 @@ impl<'a> SourceMap<'a> {
         mut patches: Vec<Patch<'b>>,
         fold: bool,
     ) -> Option<SplicedLines<'b>> {
-        fn push_trailing(
-            buf: &mut String,
-            line_opt: Option<&str>,
-            lo: &Loc,
-            hi_opt: Option<&Loc>,
-        ) -> usize {
-            let mut line_count = 0;
+        fn push_trailing(buf: &mut String, line_opt: Option<&str>, lo: &Loc, hi_opt: Option<&Loc>) {
             // Convert CharPos to Usize, as CharPose is character offset
             // Extract low index and high index
             let (lo, hi_opt) = (lo.char, hi_opt.map(|hi| hi.char));
@@ -396,18 +390,10 @@ impl<'a> SourceMap<'a> {
                     let hi_opt = hi_opt.and_then(|hi| line.char_indices().map(|(i, _)| i).nth(hi));
                     match hi_opt {
                         // If high index exist, take string from low to high index
-                        Some(hi) if hi > lo => {
-                            // count how many '\n' exist
-                            line_count = line[lo..hi].matches('\n').count();
-                            buf.push_str(&line[lo..hi]);
-                        }
+                        Some(hi) if hi > lo => buf.push_str(&line[lo..hi]),
                         Some(_) => (),
                         // If high index absence, take string from low index till end string.len
-                        None => {
-                            // count how many '\n' exist
-                            line_count = line[lo..].matches('\n').count();
-                            buf.push_str(&line[lo..]);
-                        }
+                        None => buf.push_str(&line[lo..]),
                     }
                 }
                 // If high index is None
@@ -415,7 +401,6 @@ impl<'a> SourceMap<'a> {
                     buf.push('\n');
                 }
             }
-            line_count
         }
 
         let source_len = self.source.len();
@@ -474,20 +459,11 @@ impl<'a> SourceMap<'a> {
         for part in &trimmed_patches {
             let (cur_lo, cur_hi) = self.span_to_locations(part.span.clone());
             if prev_hi.line == cur_lo.line {
-                let mut count = push_trailing(&mut buf, prev_line, &prev_hi, Some(&cur_lo));
-                while count > 0 {
-                    highlights.push(core::mem::take(&mut line_highlight));
-                    acc = 0;
-                    count -= 1;
-                }
+                push_trailing(&mut buf, prev_line, &prev_hi, Some(&cur_lo));
             } else {
                 acc = 0;
                 highlights.push(core::mem::take(&mut line_highlight));
-                let mut count = push_trailing(&mut buf, prev_line, &prev_hi, None);
-                while count > 0 {
-                    highlights.push(core::mem::take(&mut line_highlight));
-                    count -= 1;
-                }
+                push_trailing(&mut buf, prev_line, &prev_hi, None);
                 // push lines between the previous and current span (if any)
                 for idx in prev_hi.line + 1..(cur_lo.line) {
                     if let Some(line) = self.get_line(idx) {
